483e714f73a0410b38f36f558ea054af8e7d6b00
MariaAbdallah <maria.safwat2018@gmail.com>
2023-07-14 08:25:33 -0700

    0x01 C-variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Prints alphabet.
+* Return: 0
+**/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
